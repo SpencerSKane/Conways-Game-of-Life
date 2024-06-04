a816d74e378a46c058d03b57998f08da2e610f37
SpencerSKane <spencerkane.cs@gmail.com>
2024-06-04 13:58:05 -0500

    ready to run

--- ConwaysGame.py
+++ ConwaysGame.py
@@ -1,6 +1,16 @@
+import sys, argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+ON = 255
+OFF = 0
+vals = [ON, OFF]
+
+def randomGrid(N):
+    """returns a grid of NxN random values"""
+    return np.random.choice(vals, N*N, p=[0.2,0.8]).reshape(N,N)
+
 
 # represent 9x9 grid
 
@@ -19,35 +29,82 @@ N = 4
 
 # initial condition: Particular
 def addGlider(i,j,grid):
+    """adds a glider with top-left cell at (i, j)"""
     # glider moves steadily across grid
     glider = np.array([[0,0,255],[255,0,255],[0,255,255]])
     # copy pattern array into simulations 2D grid
     grid[i:i+3, j:j+3] = glider
-# NxN array of 0s
-grid = np.zeros(N*N).reshape(N,N)
-# initialize grid with glider pattern
-addGlider(1,1, grid)
-
-# display matrix as image
-# nearest makes cell edges sharper
-plt.imshow(x, interpolation='nearest')
-
-# TODO:
-# handles wrap around on the right side
-# right = grid[i][(j+1)%N]
-# handles wrap around on the top side
-# right = grid[(i+1)%N][j]
-
-# Conway's Rules
-if grid[i,j] == ON:
-    # ON cell turns OFF if it has fewer than 2 neighbors that are ON of if it has more than 3 neighbors that are ON
-    if (total < 2) or (total > 3):
-        newGrid[i,j] = OFF
+    
+def update(frameNum, img, grid, N):
+    # copy grid since we reguire 8 neighbors for calculation
+    # go line by line
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            # compute 8 neighbor sum using toroidal boundary conditions
+            # x and y should "wrap around"
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                         grid[(i-1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N] +
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+            # apply Conway's rules
+            if grid[i,j] == ON:
+                # ON cell turns OFF if it has fewer than 2 neighbors that are ON of if it has more than 3 neighbors that are ON
+                if (total < 2) or (total > 3):
+                    newGrid[i,j] = OFF
+            else:
+                if total == 3:
+                    # only applies to off cells
+                    newGrid[i,j] = ON
+                    
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    
+def main():
+    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+    parser.add_argument('--grid-size', dest='N', required=False)
+    parser.add_argument('--mov-file', dest='movFile', required=False)
+    parser.add_argument('--interval', dest='interval', required=False)
+    parser.add_argument('--glider', dest='store_true', required=False)
+    parser.add_argument('--gosper', dest='store_true', required=False)
+    args = parser.parse_args()
+    
+    # set grid size
+    N = 100
+    if args.N and int(args.N) > 8:
+        N = int(args.N)
+        
+    # set animation update interval
+    updateInterval = 50
+    if args.interval:
+        updateInterval = int(args.interval)
+        
+    #declare grid
+    grid = np.array([])
+    # check if glider demo flag is specified
+    if args.glider:
+        grid = np.zeros(N*N).reshape(N,N)
+        addGlider(1, 1, grid)
     else:
-        if total == 3:
-            # only applies to off cells
-            newGrid[i,j] = ON
-
-# output
-plt.show()
+        # populate grid with random on/off
+        grid = randomGrid(N)
+    
+    #set up animation
+    fig,ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                                  frames=10,
+                                  interval=updateInterval,
+                                  save_count=50)
+    # number of frames?
+    # set the output file
+    if args.movfile:
+        ani.save(args.mofile, fps=30, extra_args=['-vcodec', 'libx264'])
+    # output
+    plt.show()
+    
+# call main
+if __name__ == '__main__':
+    main()
 
